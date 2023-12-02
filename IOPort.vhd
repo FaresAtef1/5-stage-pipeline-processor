43d5d898964623347b056ffb986c1abcbf3d07d8
fares <96792115+FaresAtef1@users.noreply.github.com>
2023-12-02 14:52:07 +0200

    modify: remove clk from IOPort

--- IOPort.vhd
+++ IOPort.vhd
@@ -5,8 +5,7 @@ use std.textio.all;
 use ieee.std_logic_textio.all;
 
 ENTITY IOPort IS
-    PORT (CLK: IN STD_LOGIC;
-          RESET, OutW, InR: IN STD_LOGIC;
+    PORT (RESET, OutW, InR: IN STD_LOGIC;
           Write_Data: IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- Data to be written to the output port
           InData: IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- Data from the input port
           Read_Data: OUT STD_LOGIC_VECTOR (31 DOWNTO 0)); -- Data read from the input port
@@ -16,19 +15,16 @@ ARCHITECTURE ArchIOPort OF IOPort IS
     SIGNAL INPort : STD_LOGIC_VECTOR(31 DOWNTO 0);
     SIGNAL OUTPort : STD_LOGIC_VECTOR(31 DOWNTO 0);
 BEGIN
-    PROCESS (CLK, RESET, InData)
+    PROCESS (RESET, OutW, InR, Write_Data, InData, INPort)
     BEGIN
         IF RESET = '1' THEN
             INPort <= (OTHERS => '0');
             OUTPort <= (OTHERS => '0');
             Read_Data <= (OTHERS => '0');
-        ELSIF RISING_EDGE(CLK) THEN
-            IF OutW = '1' THEN
-                OUTPort <= Write_Data;
-            END IF;
-            IF InR = '1'THEN
-                Read_Data <= INPort;
-            END IF;    
+        ELSIF OutW = '1' THEN
+            OUTPort <= Write_Data;
+        ELSIF InR = '1'THEN
+            Read_Data <= INPort;
         END IF;
         IF RESET = '0' THEN
             INPort <= InData;
