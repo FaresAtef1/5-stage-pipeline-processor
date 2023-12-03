00886faf23f485e8cfe3520b01323829f7b224b9
Ghaithq <gaoia123@gmail.com>
2023-12-03 20:29:52 +0200

    fix: fixed PC Decoder

--- PCDecoder.vhd
+++ PCDecoder.vhd
@@ -10,26 +10,25 @@ ENTITY PCDecoder IS
         Rds : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         memory : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         PC_inc : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        correct_branch : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        PC_inc_mem : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         call : IN STD_LOGIC;
+        Zero_Flag : IN STD_LOGIC;
         mem_to_PC : IN STD_LOGIC;
-        prediction : IN STD_LOGIC;
         branch : IN STD_LOGIC;
-        wrong_pred : IN STD_LOGIC;
         prev_branch : IN STD_LOGIC;
         PC_out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0));
 END PCDecoder;
 
 ARCHITECTURE PCDecoderArch OF PCDecoder IS
 BEGIN
-    PROCESS (Rds, correct_branch, memory, PC_inc, call, mem_to_PC, prediction, branch, wrong_pred, prev_branch)
+    PROCESS (Rds, PC_inc_mem, memory, PC_inc, call, mem_to_PC, branch, prev_branch)
     BEGIN
-        IF (call = '1') OR (branch = '1' AND prediction = '1') THEN
+        IF (call = '1') OR (branch = '1') THEN
             PC_out <= Rds;
         ELSIF mem_to_PC = '1' THEN
             PC_out <= memory;
-        ELSIF wrong_pred = '1' AND prev_branch = '1' THEN
-            PC_out <= correct_branch;
+        ELSIF Zero_Flag = '0' AND prev_branch = '1' THEN
+            PC_out <= PC_inc_mem;
         ELSE
             PC_OUT <= PC_inc;
         END IF;
